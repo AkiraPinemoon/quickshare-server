2ab029c17c3751e9fc6598e08dcb9e29844a8508
Akira Pinemoon <paulik080705@gmail.com>
2023-03-24 11:48:40 +0100

    added routes for frontend

--- index.js
+++ index.js
@@ -1,26 +1,17 @@
 
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000;
 
-const sqlite = require('sqlite3')
-const db = new sqlite.Database('db.sqlite');
-
-db.serialize(() => {
-    db.run("CREATE TABLE ids (id: TEXT, ttl: INT)");
-})
-
-const dbAdd = db.prepare("INSERT INTO ids VALUES (?, 60)");
-const dbKeep = db.prepare("UPDATE ids SET ttl = 60 WHERE id = ?");
-const dbRemove = db.prepare("DELETE FROM ids WHERE id = ?");
-const dbCheck = db.prepare("SELECT COUNT(*) FROM ids WHERE id = ?");
-
-app.get("/", (req, res) => {
-    res.send("cat");
-})
+app.use(express.static("../dist"));
 
 app.get("/api/isValid/:fileId", (req, res) => {
     dbCheck.get((row)=>{console.log(row)})
+});
+
+app.get("/*", (req, res) => {
+    res.sendFile("dist/index.html", { root: ".." });
 });
 
 app.listen(port, ()=>{
