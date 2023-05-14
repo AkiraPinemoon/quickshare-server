bc14ed9938b5fc073468cae6c72b353aae3aed02
Paul Kunkel <paulik080705@gmail.com>
2023-05-14 20:09:28 +0200

    hotfix

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const port = 80;
 
-77app.use(require("cors")())
+//app.use(require("cors")())
 
 const sqlite3 = require('sqlite3').verbose();
 
